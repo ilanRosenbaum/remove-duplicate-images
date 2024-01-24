3300aabe94e55fb8228596182ddf01a1912d7c99
IlanRosenbaum <ilan@al.net>
2024-01-24 15:01:04 -0600

    Optimize algorithm

--- src/delete_duplicates.py
+++ src/delete_duplicates.py
@@ -50,10 +50,13 @@ def get_video_phash(video_path: str):
         print(f"Error processing video {video_path}: {e}")
         return None
 
-def populate_hashes(base_path, folders: dict):
+def populate_and_remove_duplicates(base_path, folders: dict):
     """
-    Populate a dictionary with the phashes of all files in the given folders.
+    Populate a dictionary with the phashes of all files in the given folders, 
+    then remove all duplicate images and videos that are not in the priority folders.
     """
+
+    files_removed = 0
     hashes = {}
     for folder_subpath, folder_priority in folders.items():
         folder_path = os.path.join(base_path, folder_subpath)
@@ -64,45 +67,25 @@ def populate_hashes(base_path, folders: dict):
         for root, _, files in os.walk(folder_path):
             for file in files:
                 file_path = os.path.join(root, file)
-                file_hash_with_priority = (get_media_phash(file_path), folder_priority)
+                file_hash_with_priority = [get_media_phash(file_path), folder_priority, file_path]
 
                 if file_hash_with_priority[0] is None:
                     continue
 
-                if file_hash_with_priority[0] not in hashes.keys() or file_hash_with_priority[1] < hashes[file_hash_with_priority[0]]:
-                    hashes[file_hash_with_priority[0]] = file_hash_with_priority[1]
-    return hashes
-
-def remove_non_priority_duplicates(base_path: str, folders: dict, hashes: dict):
-    """
-    Remove all duplicate images and videos that are not in the priority folders.
-    """
-    files_removed = 0
-
-    for folder_subpath in folders.keys():
-        folder_path = os.path.join(base_path, folder_subpath)
-        if not os.path.exists(folder_path):
-            continue
-
-        for root, _, files in os.walk(folder_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                file_hash = get_media_phash(file_path)
-
-                if file_hash is None or (file_hash in hashes and hashes[file_hash] != folders[folder_subpath]):
+                if file_hash_with_priority[0] not in hashes.keys():
+                    # If a picture or video with the same hash doesn't exist, add it to the dictionary
+                    hashes[file_hash_with_priority[0]] = [file_hash_with_priority[1], file_hash_with_priority[2]]      
+                elif file_hash_with_priority[1] < hashes[file_hash_with_priority[0]][0]:
+                    # If a picture or video with the same hash exists, but the new one has a higher priority, remove the old one
+                    os.remove(hashes[file_hash_with_priority[0]][1])
+                    files_removed += 1
+                    hashes[file_hash_with_priority[0]] = [file_hash_with_priority[1][0], file_path]
+                else:
+                    # If a picture or video with the same hash exists, but the old one has a higher priority, remove the new one
                     os.remove(file_path)
                     files_removed += 1
-                    print(f"Removed: {file_path}")
-        
-    return files_removed
 
-def populate_and_remove_duplicates(base_path, folders: dict):
-    """
-    Populate a dictionary with the phashes of all files in the given folders, 
-    then remove all duplicate images and videos that are not in the priority folders.
-    """
-    hashes = populate_hashes(base_path, folders)
-    return remove_non_priority_duplicates(base_path, folders, hashes)
+    return files_removed
 
 def main():
     """
