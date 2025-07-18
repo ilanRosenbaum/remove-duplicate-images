d25939d884124f7e959e12de13b1f77edf7b48ac
IlanRosenbaum <ilan@al.net>
2025-07-17 19:20:18 -0500

    Change license

--- test/delete_duplicates_test.py
+++ test/delete_duplicates_test.py
@@ -1,4 +1,14 @@
-# scripts/test.py
+# Ilan's Website
+# Copyright (C) 2024-2025 ILAN ROSENBAUM
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 
 import unittest
 import os
